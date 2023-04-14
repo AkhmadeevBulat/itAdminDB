b5df509e1041b72991df8a6c67c2fb4219262949
Akhmadeev Bulat Nailevich <bulat-axxxmadeev@mail.ru>
2023-04-14 11:50:30 +0300

    ...

---
+++ cmd2.0/itAdminDB2.0.py
@@ -0,0 +1,12 @@
+import os
+from time import sleep
+from classes import LDAPUserVerification, SQLManager
+from functions import developer, inputEndExit, inputContinue, readFile, clear, beautifulTable, parse_string
+import pymysql.err
+
+
+def main():
+    pass
+
+
+
